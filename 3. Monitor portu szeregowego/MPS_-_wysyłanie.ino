3d4b2a029f6ef0c12b793b77acd4d751779466b1
Ariel Antonowicz <35267806+Arillos@users.noreply.github.com>
2022-02-21 11:05:30 +0100

    Update MPS_-_wysyłanie.ino

--- 3. Monitor portu szeregowego/MPS_-_wysyłanie.ino
+++ 3. Monitor portu szeregowego/MPS_-_wysyłanie.ino
@@ -1,11 +1,15 @@
 void setup() {
   Serial.begin(9600);  // Uruchomienie komunikacji
+  Serial.print("Dzień dobry EKOS!");    // Wyświetlenie w MPS: Dzień dobry EKOS!   (bez znaku nowej lini)
+  Serial.print("\n");                   // Dodanie znaku nowej lini
+  delay(1000);                          // Odczekaj 1 sekundę
+  Serial.println("Dobranoc EKOS!");     // Wyświetlenie w MPS: Dobranoc EKOS! (wraz ze znakiem nowej lini) 
+  delay(1000);                          // Odczekaj 1 sekundę
 }
 
 void loop() {
-
-  if (Serial.available() > 0)                             // Jeżeli Arduino odebrało dane to ..
-  {       
-      Serial.println(Serial.readStringUntil('\n'));       // Odczytaj wszystkie znaki do momentu wystąpienia znaku nowej lini
-  }
+  
+  Serial.println("Tekst powtarzający się co sekundę :) ");     // Wyświetlenie w MPS: Tekst potwarzający się co sekundę :)
+  delay(1000);                                                 // Odczekaj 1 sekundę
+  
 }
